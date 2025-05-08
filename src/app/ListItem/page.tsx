b199363dbc17040c6bab51fb0fce061f296aa593
Salman <salmanalthof31@gmail.com>
2025-05-08 10:37:14 +0700

    refactor: move complex element to component for easy maintenance

--- src/app/ListItem/page.tsx
+++ src/app/ListItem/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 import { useState, useEffect, ChangeEvent } from "react";
 import Navbar from "../components/navbar";
-// import { getStaticProps } from "next/dist/build/templates/pages";
-
-// import Image from "next/image";
+import CardProduct from "../components/ListItem/card-products";
+import CategoryList from "../components/ListItem/Category-list";
 
 export default function ListItem() {
     type Category = {
@@ -64,34 +63,14 @@ export default function ListItem() {
     return (
       <div className="bg-amber-700 text-center">
             <Navbar />
-            <input className="mt-[8rem] text-center bg-white/60 rounded-md hover:bg-amber-200 p-1" placeholder="Search" onChange={getInputSearch}></input> <button className="ml-2 bg-yellow-300 p-1 rounded-md hover:bg-yellow-500 hover:text-amber-50 transition-opacity active:scale-90 duration-200" onClick={handleSearch}>Search</button>
-            <main className="flex flex-row p-15 px-[12%]">
+            <input className="mt-[8rem] text-center bg-white/60 rounded-md hover:bg-amber-200 p-1" placeholder="Search" onChange={getInputSearch}></input> <button className="font-bold ml-2 bg-yellow-300 p-1 rounded-md hover:bg-yellow-500 hover:text-amber-50 transition-opacity active:scale-90 duration-200" onClick={handleSearch}>Search</button>
+            <main className="flex flex-row p-15 px-[12%] gap-5">
                 {/* section category */}
-                <section className="p-2 w-[12rem] bg-amber-50 rounded-2xl h-[10rem]">
-                    <h1>Category Products</h1>
-                    <div className="flex flex-col items-start">
-                        <button className="p-1 text-gray-700 hover:bg-gray-100 focus:bg-emerald-500 focus:text-white" onClick={() => handleCategory("All")}>All</button>
-                        <button className="p-1 text-gray-700 hover:bg-gray-100 focus:bg-emerald-500 focus:text-white" onClick={() => handleCategory("Electronics")}>Electronics</button>
-                        <button className="p-1 text-gray-700 hover:bg-gray-100 focus:bg-emerald-500 focus:text-white" onClick={() => handleCategory("Clothes")}>Clothes</button>
-                    </div>
-                </section>
+                <CategoryList handleCategory = {handleCategory} />
                 {/* section card product */}
-                <section className="min-h-screen flex flex-row justify-center items-center flex-wrap gap-[2rem] w-full">
+                <section className="min-h-screen flex flex-row flex-wrap gap-[2rem] w-full">
                     {products.map ((product) => (
-                        <div key={product?.id} className="w-[25rem] bg-amber-50 rounded-2xl relative">
-                            <img src={product?.images?.[0] || "/no-img.jpg"} className="rounded-t-2xl w-full h-[20rem] object-cover"/>
-                            <div className="p-3 flex flex-col justify-between min-h-[16rem] text-justify">
-                                <div>
-                                    <h1 className="text-lg font-bold">{product.title}</h1>
-                                    <h3 className="mb-8 text-sm text-gray-400">Category: {product?.category?.name}</h3>
-                                    <p className="text-justify">{product?.description?.slice(0,100)}...</p>
-                                </div>
-                                <div className="text-center">
-                                    <p>${product?.price}</p>
-                                    <button className="p-2 rounded-md bg-emerald-500 hover:bg-emerald-700 hover:text-amber-50 transition-opacity delay-200 active:scale-90">Product Detail</button>
-                                </div>
-                            </div>
-                        </div>
+                        <CardProduct product={product} />
                     ))}        
                 </section>
             </main>
---
+++ src/app/components/ListItem/card-products.tsx
@@ -0,0 +1,38 @@
+type Category = {
+    id: number;
+    name: string;
+    image: string;
+}
+type Product = {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    images: string[];
+    category: Category;
+}
+
+interface CardProductProps {
+    product: Product;
+}
+export default function CardProduct ({ product }: CardProductProps) {
+    return (
+        <div className="w-[25rem] bg-amber-50 rounded-2xl h-[42rem]">
+            <img src={product?.images?.[0] || "/no-img.jpg"} className="rounded-t-2xl w-full h-[25rem] object-cover"/>
+            <div className="p-3 flex flex-col justify-between min-h-[16rem] text-justify">
+            <div>
+                <h1 className="text-lg font-bold text-start">{product.title}</h1>
+                <h3 className="mb-8 text-sm text-gray-400">Category: {product?.category?.name}</h3>
+                <p className="text-justify">{product?.description?.slice(0,100)}...</p>
+            </div>
+            <div className="text-center">
+                <p>${product?.price}</p>
+                <button className="p-2 rounded-md bg-emerald-500 hover:bg-emerald-700 hover:text-amber-50 transition-opacity delay-200 active:scale-90">Product Detail</button>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+// export default CardProduct;
+
