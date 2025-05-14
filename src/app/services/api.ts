f9f602cb3d15867c5d651f03f93a9d059765261d
Salman <salmanalthof31@gmail.com>
2025-05-14 16:14:54 +0700

    chore & refactor: create api.ts contain all interface and fetching for all page

---
+++ src/app/services/api.ts
@@ -0,0 +1,33 @@
+export interface Category {
+    id: number;
+    name: string;
+    image: string;
+    slug: string;
+    creationAt: string;
+    updatedAt: string;
+}
+export interface Product {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    images: string[];
+    category: Category;
+}
+
+const API_BASE_URL = 'https://api.escuelajs.co/api/v1'
+// fetching data products
+export async function fetchDataProd (): Promise<Product[]> {
+    const response: Response = await fetch(`${API_BASE_URL}/products`)
+    return response.json();
+}
+// fetching data category
+export async function fetchDataCat (): Promise<Category[]> {
+    const response: Response = await fetch(`${API_BASE_URL}/categories`)
+    return response.json();
+}
+// fetchind data single product
+export async function fetchDataSingProd (id: string): Promise<Product> {
+    const response: Response = await fetch(`${API_BASE_URL}/products/${id}`)
+    return response.json();
+}
