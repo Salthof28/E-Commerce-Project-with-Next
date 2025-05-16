db255fc06b95ebaeb43af953e97157c403aeaccb
Salman <salmanalthof31@gmail.com>
2025-05-17 06:21:13 +0700

    refactor: change color text for Category Product

--- src/app/components/ListItem/Category-list.tsx
+++ src/app/components/ListItem/Category-list.tsx
@@ -5,7 +5,7 @@ interface CategoryFilterProps {
 export default function CategoryList ({handleCategory}: CategoryFilterProps) {
     return (
         <section className="p-2 w-[9rem] xl:w-[18rem] 2xl:w-[16rem] bg-amber-50 rounded-2xl h-full text-start hidden lg:block">
-            <h2 className="text-[0.8rem] xl:text-[1rem]">Category Product</h2>
+            <h2 className="text-[0.8rem] xl:text-[1rem] text-black font-bold mb-[1rem]">Category Product</h2>
             <div className="flex flex-col items-start">
                 <button className="p-1 text-black hover:bg-gray-100 focus:bg-emerald-500 focus:text-white text-[0.8rem] xl:text-[1rem]" onClick={() => handleCategory("All")}>All</button>
                 <button className="p-1 text-black hover:bg-gray-100 focus:bg-emerald-500 focus:text-white text-[0.8rem] xl:text-[1rem]" onClick={() => handleCategory("Electronics")}>Electronics</button>
