f23da4c21bc454a6d263a1b5e060e0511baf0b00
Salman <salmanalthof31@gmail.com>
2025-05-19 22:25:54 +0700

    refactor: change routing to ListItem page

--- src/app/components/Home/Category-home.tsx
+++ src/app/components/Home/Category-home.tsx
@@ -10,7 +10,7 @@ export default function CategoryHome ({gethandleRouter, categoryProd}: CategoryH
             <img className="absolute inset-0 w-full h-full object-cover rounded-2xl opacity-40" src={categoryProd?.image || "/no-img.jpg"} />
             <h1 className="z-10 text-amber-50 text-md md:text-xl lg:text-4xl">The Best <span className="text-xs md:text-xs lg:text-2xl">{categoryProd?.name}</span></h1>
             <h3 className="z-10 text-amber-50 text-xs md:text-xs lg:text-xl">Waiting For You</h3>
-            <button className="z-10 bg-yellow-500 p-1 text-black rounded-md hover:bg-yellow-500 hover:text-amber-50 active:scale-80 duration-200 text-xs md:text-sm lg:text-md" onClick={() => gethandleRouter(categoryProd?.name)}>Shop {categoryProd?.name}</button>
+            <button className="z-10 bg-yellow-500 p-1 text-black rounded-md hover:bg-yellow-500 hover:text-amber-50 active:scale-80 duration-200 text-xs md:text-sm lg:text-md" onClick={() => gethandleRouter(categoryProd?.slug)}>Shop {categoryProd?.name}</button>
         </div>
     );
 }
