878bd46ca8d8c794b4be28cbe6d7ff89f58be2c1
Salman <salmanalthof31@gmail.com>
2025-05-30 00:47:40 +0700

    feat: dashboard show total product by category

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,90 @@
 'use client'
+import useAuthAdmin from "@/hooks/useAuthAdmin";
 import NavigationAdmin from "@/components/Dashboard/navigation";
 import AdminPanel from "@/components/Dashboard/adminPanel";
-import useAuthAdmin from "../../hooks/useAuthAdmin";
+import { Plus, SquarePen, Trash2 } from "lucide-react";
+import { fetchDataCat, fetchDataProdPag, CountLengDataProd, fetchDataProd } from "@/services/api";
+import { Category, Product, FormDataProduct } from "@/types/interfaces";
+import { useEffect, useState } from "react";
+import ProductForm from "@/components/Dashboard/ProductForm";
+import HeaderTable from "@/components/Dashboard/HeaderTable";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function dashboard () {
     const { session } = useAuthAdmin();
+    const [products, setProducts] = useState<Product[]>([]);
+    const [category, setCategory] = useState<Category[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+
+     // variabel paganation page
+
+    const [totalPoduct, setTotalProduct] = useState<number>(0);
+    
+    
+    const fetchDataProducts = async () => {
+        try{
+            setLoading(true);
+            const data: Product[] = await fetchDataProd();
+            const dataCat: Category[] = await fetchDataCat();
+            const dataNumber: number = data.length;
+            setTotalProduct(dataNumber)
+            setProducts(data);
+            setCategory(dataCat);
+            setLoading(false);
+        }
+        catch (err: unknown) {
+            setError(err instanceof Error ? err.message : 'An unknown error occurred');
+            console.log(`Error: ${error}`)
+        }
+    }
+    useEffect (() => {
+        fetchDataProducts();
+    }, []);
+
+    const persentaseProduct = (totalSingProd: number): number => {
+        const persenProduct: number = (totalSingProd/totalPoduct)*100;
+        return persenProduct;
+    }
+    const totalProductbyCat =() => {
+        const result = category.map((cat) => {
+            const total = products.filter((prod) => prod.category.id === cat.id).length;
+            return {categoryName: cat.name, Total: total};
+    });
+    return result
+    }
+    console.log(totalProductbyCat());
     return (
         <div className="bg-[rgb(8,5,3)] min-h-screen overflow-x-hidden">
             <NavigationAdmin session={session} />
-            
-            <main className="flex flex-row text-[rgb(240,230,226)]">
+            <main className="flex flex-row text-[rgb(240,230,226)] min-h-screen">
                 {/* for panel */}
-                <section className="w-[15%]">
+                <section className="lg:w-[15%]">
                     <AdminPanel session={session} />
                 </section>
                 {/* for content */}
-                <section className="w-[85%] p-[2rem] px-[2rem] xl:px-[4rem] 2xl:px-[20rem]">
-                    <h1>PAGE DASHBOARD</h1>
-                    <h1>{session?.user?.name}</h1>
-                    <h1>{session?.user?.email}</h1>
+                <section className="flex flex-col w-[100%] lg:w-[85%] p-[2rem] text-[rgb(240,230,226)] items-center px-[0.5rem] md:px-[10rem] xl:px-[20rem] 2xl:px-[40rem] gap-[1rem]">
+                    <h1 className="text-[1rem] md:text-[1.5rem] xl:text-[2rem] font-bold">Dashboard</h1>
+                    {/* section persentase product */}
+                    <section className="flex flex-row min-w-full gap-[1rem]">
+                        {/* card 1 */}
+                        <section className="flex flex-col gap-[1rem] p-[1rem] overflow-hidden rounded-[0.4rem] border border-gray-500/40 w-full">    
+                            <h1 className="text-[1rem] md:text-[1.2rem] xl:text-[1.5rem] font-bold">Products</h1>
+                            {totalProductbyCat().map((item) => (
+                            <div className="flex flex-col gap-[0.5rem]">
+                                <p className="text-[0.6rem] md:text-[0.8rem] xl:text-[1rem] font-bold">{item.categoryName}</p>
+                                <div className="bg-gray-200 w-full h-[0.5rem] rounded-[0.5rem]">
+                                    <div className={`bg-emerald-600 h-[0.5rem] rounded-[0.5rem]`} style={{ width: `${persentaseProduct(item.Total)}%` }}></div>
+                                </div>
+                            </div>
+                            ))}
+                        </section>
+                    </section>
+                    {loading && (
+                    <section className="flex justify-center items-center w-full h-full">
+                        <h1 className="text-lg lg:text-xl xl:text-2xl 2xl:text-4xl font-bold text-amber-50">Loading....</h1>
+                    </section>
+                    )}
                 </section>
             </main>
         </div>
