ddb7f627bfb98265c5b922be68d046754feefc2e
Salman <salmanalthof31@gmail.com>
2025-06-04 14:51:14 +0700

    testing: testing hook for authenticated

---
+++ src/__tests__/hooks/useAuthCustomer.test.tsx
@@ -0,0 +1,50 @@
+import useAuthCustomer from "@/hooks/useAuthCustomer";
+import { renderHook } from "@testing-library/react";
+import { useSession } from "next-auth/react";
+import * as nextNavigation from "next/navigation";
+jest.mock('next-auth/react', () => ({
+    useSession: jest.fn()
+}));
+jest.mock('next/navigation', () => ({
+    useRouter: jest.fn()
+}));
+
+describe('testing hook useAuthAdmin', () => {
+
+    const pushMock = jest.fn(); 
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        const mockedUseRouter = nextNavigation.useRouter as jest.Mock;
+        mockedUseRouter.mockReturnValue({ push: pushMock });
+        (useSession as jest.Mock).mockReturnValue({
+            data: null,
+            status: 'unauthenticated',
+        });
+    });
+
+    test('redirect to /login when unauthenticated', () => {
+        renderHook(() => useAuthCustomer());
+        expect(pushMock).toHaveBeenCalledWith('/login');
+    });
+
+    test('redirect to /login when unauthenticated', () => {
+        (useSession as jest.Mock).mockReturnValue({
+            data: {
+                user: {
+                    id: 2,
+                    email: 'dude@Mail.com',
+                    password: 'dudewadude',
+                    name: 'Super Dude',
+                    role: 'admin',
+                    avatar: 'images/dude.png',
+                },
+                accessToken: 'tokek-123'
+            },
+            status: 'authenticated',
+        });
+
+        renderHook(() => useAuthCustomer());
+        expect(pushMock).toHaveBeenCalledWith('/dashboard');
+    });
+});
